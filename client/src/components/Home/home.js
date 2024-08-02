91b6a5df8b8ead7724191cfebc3431c48e968906
bogdan9876 <bogdankryveiko@gmail.com>
2024-08-02 17:08:19 +0300

    demo html

--- client/src/components/Home/home.js
+++ client/src/components/Home/home.js
@@ -1,12 +1,28 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
 import './home.css';
 
 function Home() {
-  
   return (
-    <div>
-      
+    <div className="home-container">
+      <div className="left-panel">
+        <div className="left-panel-header">
+        </div>
+        <div className="left-panel-main">
+          <div className="left-panel-chats-word" >
+            Chats
+          </div>
+          <div className="left-panel-chats-list">
+          </div>
+        </div>
+      </div>
+      <div className="right-panel">
+        <div className="right-panel-header">
+        </div>
+        <div className="right-panel-main">
+        </div>
+        <div className="right-panel-message-input">
+        </div>
+      </div>
     </div>
   );
 }
