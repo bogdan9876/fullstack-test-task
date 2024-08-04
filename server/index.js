b647cfb355ff944cb0d4f4f1174f859efeee33bb
bogdan9876 <bogdankryveiko@gmail.com>
2024-08-04 21:54:26 +0300

    fix deploy

--- server/index.js
+++ server/index.js
@@ -6,23 +6,19 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes');
-
 const app = express();
-const PORT = 5000;
-const MONGO_URI = process.env.MONGO_URI;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-mongoose.connect(MONGO_URI, {
-  serverSelectionTimeoutMS: 5000,
-})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err.message));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 app.use('/api/users', userRoutes);
 app.use('/api/chats', chatRoutes);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
